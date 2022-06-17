7cc1eb4c3e283ab4fda4b69d2205e5291d6c2697
IliartKersam <101018828+IliartKersam@users.noreply.github.com>
2022-06-17 14:07:39 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 600
+RETRY_TIME = 300
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
